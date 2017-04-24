d4de24f7a4c4a7d96531f0fee979270e073e6a43
Lucas Romao <lucas.romao.silva@gmail.com>
2017-04-23 23:13:29 -0300

    lines for the pthread implementation

--- src/run_measurements.sh
+++ src/run_measurements.sh
@@ -39,9 +39,21 @@ for NAME in ${NAMES[@]}; do
             NTHREADS=$(($NTHREADS * 2))
             SIZE=$INITIAL_SIZE
         done
+    else
+        while [ $NTHREADS -le 32 ]; do
+            for ((i=1; i<=$ITERATIONS; i++)); do
+                    perf stat -r $MEASUREMENTS ./$NAME -2.5 1.5 -2.0 2.0 $SIZE >> full_$NTHREADS.log 2>&1
+                    perf stat -r $MEASUREMENTS ./$NAME -0.8 -0.7 0.05 0.15 $SIZE >> seahorse_$NTHREADS.log 2>&1
+                    perf stat -r $MEASUREMENTS ./$NAME 0.175 0.375 -0.1 0.1 $SIZE >> elephant_$NTHREADS.log 2>&1
+                    perf stat -r $MEASUREMENTS ./$NAME -0.188 -0.012 0.554 0.754 $SIZE >> triple_spiral_$NTHREADS.log 2>&1
+                    SIZE=$(($SIZE * 2))
+            done
+            NTHREADS=$(($NTHREADS * 2))
+            SIZE=$INITIAL_SIZE
+        done
     fi
     SIZE=$INITIAL_SIZE
-
+    NTHREADS=$INITIAL_NTHREADS
     mv *.log results/$NAME
     rm output.ppm
 done
