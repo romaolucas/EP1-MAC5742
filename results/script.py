69a674f3e497e28202456b51e89826951c4d61f7
Isabella Mendonça <isaisaisa@live.dk>
2017-05-01 19:22:57 -0300

    fixing identation problems

--- results/script.py
+++ results/script.py
@@ -1,25 +1,25 @@
 import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
 from parse import *
 
 data = {}
-data_list = []
 
 list_of_files = glob.glob('mandelbrot_omp/*.log')
 for file_name in list_of_files:
-	FI = open(file_name, 'r')
-	print file_name
-	#FO = open(file_name.replace('log', 'out'), 'w') 
-	for line in FI:
-		r = search('{clock}      task-clock (msec)         #    {cpu} CPUs utilized', line)
-		if r:
-			data['clock'] = float(r['clock']) 
-			data['cpu'] = float(r['cpu'])
-		r = search('{time} seconds time elapsed', line)
-		if r:
-			data['time'] = float(r['time'])
-			data_list.append(data.copy())
-
-print data_list
+    FI = open(file_name, 'r')
+    data_list = []
+    #FO = open(file_name.replace('log', 'out'), 'w') 
+    for line in FI:
+        r = search('{clock}      task-clock (msec)         #    {cpu} CPUs utilized', line)
+        if r:
+            data['clock'] = float(r['clock']) 
+            data['cpu'] = float(r['cpu'])
+        r = search('{time} seconds time elapsed', line)
+        if r:
+            data['time'] = float(r['time'])
+            data_list.append(data.copy())
 
 FI.close()
 #FO.close()
