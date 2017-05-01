f0224679269ffcf8599e7a8b463b90e4a836f1ca
Lucas Romao <lucas.romao.silva@gmail.com>
2017-05-01 19:57:03 -0300

    testing plotting values

--- results/script.py
+++ results/script.py
@@ -20,6 +20,27 @@ for file_name in list_of_files:
         if r:
             data['time'] = float(r['time'])
             data_list.append(data.copy())
+    FI.close()
 
-FI.close()
+data_list = []
+FI = open("mandelbrot_seq/full.log")
+for line in FI:
+    data = {}
+    r = search('{clock}      task-clock (msec)         #    {cpu} CPUs utilized', line)
+    if r:
+        data['clock'] = float(r['clock']) 
+        data['cpu'] = float(r['cpu'])
+    r = search('{time} seconds time elapsed', line)
+    if r:
+        data['time'] = float(r['time'])
+        data_list.append(data.copy())
+y = [d['time'] for d in data_list]
+x = [2**i for i in range(4, 14)] 
+
+plt.xlim(16, 8192)
+plt.ylim(min(y), max(y))
+plt.autoscale(False)
+plt.plot(x, y)
+plt.show()
+
 #FO.close()
