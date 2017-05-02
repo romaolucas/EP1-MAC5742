28ed665220bfb1ea2c7f08fad1e72be539cffad6
Lucas Romao <lucas.romao.silva@gmail.com>
2017-05-01 23:29:52 -0300

    plotting for openmp and pthread

--- results/script.py
+++ results/script.py
@@ -1,18 +1,12 @@
 # encoding=utf8  
 import glob
-import numpy as np
 import matplotlib.pyplot as plt
 
 from parse import *
 
-data = {}
-
-list_of_files = glob.glob('mandelbrot_seq/*.log')
-file_times = {}
-for file_name in list_of_files:
-    FI = open(file_name, 'r')
+def get_data_list(FI):
+    data = {}
     data_list = []
-    FO = open(file_name.replace('log', 'out'), 'w') 
     for line in FI:
         r = search('{clock}      task-clock (msec)         #    {cpu} CPUs utilized', line)
         if r:
@@ -22,10 +16,20 @@ for file_name in list_of_files:
         if r:
             data['time'] = float(r['time'])
             data_list.append(data.copy())
-    file_times[file_name] = [d['time'] for  d in data_list]
+    return data_list
+
+def write_to_output(FO, file_name, times):
     import numpy as np
-    FO.write("Media dos tempos de {} : {}\n".format(file_name, np.mean(file_times[file_name])))
-    FO.write("Desvio padrao de {} : {}".format(file_name, np.std(file_times[file_name])))
+    FO.write("Media dos tempos de {} : {}\n".format(file_name, np.mean(times)))
+    FO.write("Desvio padrao de {} : {}".format(file_name, np.std(times)))
+
+list_of_files = glob.glob('mandelbrot_seq/*.log')
+file_times = {}
+for file_name in list_of_files:
+    FI = open(file_name, 'r')
+    FO = open(file_name.replace('log', 'out'), 'w') 
+    file_times[file_name] = [d['time'] for  d in get_data_list(FI)]
+    write_to_output(FO, file_name, file_times[file_name])
     FI.close()
     FO.close()
     
@@ -37,3 +41,30 @@ plt.legend()
 plt.xlabel('Tamanho da Entrada')
 plt.ylabel('Tempo de Execucao (s)')
 plt.savefig('seq.png')
+plt.close()
+
+list_of_images = ['full', 'elephant', 'seahorse', 'triple_spiral']
+num_threads = [2**i for i in range(0, 6)]
+folders = ['mandelbrot_pth', 'mandelbrot_omp']
+
+for folder in folders:
+    for image in list_of_images:
+        file_times = {}
+        for nthread in num_threads:
+            file_name = folder + "/" + image + "_" + str(nthread) + ".log"
+            FI = open(file_name, 'r')
+            FO = open(file_name.replace('log', 'out'), 'w')
+            file_times[file_name] = [d['time'] for d in get_data_list(FI)]
+            write_to_output(FO, file_name, file_times[file_name]) 
+            FI.close()
+            FO.close()
+        x = [2**i for i in range(4, 14)] 
+        for file_name in file_times:
+            plt.plot(x, file_times[file_name], label=(file_name.split('/')[1]).split('.')[0])
+        plt.legend()
+        plt.xlabel('Tamanho da Entrada')
+        plt.ylabel('Tempo de Execucao (s)')
+        plt.savefig(folder + '_' + image + '.png')
+        plt.close()
+
+           
